fb84342710b5a8a6b6fe02b94fc9c72ab7d4f299
mohsin <mohdmohsin786357@gmail.com>
2024-01-15 12:36:49 -0800

    first commit

---
+++ type.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+ans = add(12, 13)
+new_ans = add(ans, 150)
+print(ans, new_ans)
+add(12, 13) * add(12, 50) / add(23, 32)
+a = add(12, 13)
+b = add(12, 50)
+c = add(23,32)
+a * b / c
